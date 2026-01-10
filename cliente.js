c3290cb6f2ebc7648dac4895da40eaef462e21ac
Jhonnipaucar <jhonni.paucar@ister.edu.ec>
2026-01-10 01:00:47 +0100

    cliente

---
+++ cliente.js
@@ -0,0 +1,9 @@
+class Cliente {
+    constructor(id, nombre, apellido, fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
+
